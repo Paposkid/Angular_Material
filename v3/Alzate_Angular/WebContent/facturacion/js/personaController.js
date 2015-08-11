2c033fa080f99bfb7bb5b63d71dcc3c6027ee25b
Jorge A. Morales <joalmopa18@gmail.com>
2015-08-10 20:46:48 -0500

    Version 3
    Version 3

---
+++ v3/Alzate_Angular/WebContent/facturacion/js/personaController.js
@@ -0,0 +1,7 @@
+// Se registra un controlador con la variable conttollers que se creo en el archivo "app.js"
+controllers.controller("PersonaController", ['$scope', personaController]);
+
+function personaController($scope) {
+
+	
+}
